2c431e92250212cbf78da94c19add1d9b5a11c6e
Kristaps Ritins <kritinsdev@gmail.com>
2022-07-15 00:40:06 +0300

    init proj

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--description');
+const inputDuration = document.querySelector('.form__input--price');
+
+// navigator.geolocation.getCurrentPosition()
