79356ae56ec429e1155b1bc5b1fb6d3a3792eca3
Kristaps Ritins <kritinsdev@gmail.com>
2022-07-15 17:29:11 +0300

    using leaflet lib

--- script.js
+++ script.js
@@ -9,4 +9,24 @@ const inputType = document.querySelector('.form__input--type');
 const inputDistance = document.querySelector('.form__input--description');
 const inputDuration = document.querySelector('.form__input--price');
 
-// navigator.geolocation.getCurrentPosition()
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        const {latitude} = position.coords;
+        const {longitude} = position.coords;
+
+        const coords = [latitude, longitude];
+
+        const map = L.map('map').setView(coords, 14);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker(coords).addTo(map)
+            .bindPopup('This is my current location')
+            .openPopup();
+    }, function () {
+        alert('Could not get your location');
+    })
+}
+
